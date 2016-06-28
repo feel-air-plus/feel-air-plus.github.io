c5718138879dc5c76487bceb3506b46cb9ea807b
Shun Takahashi <shun.takahashi0728@gmail.com>
2016-06-28 16:50:00 +0900

    チャット一覧をマップ画面に変更。各種レイアウト修正。

--- js/chatMap.js
+++ js/chatMap.js
@@ -128,4 +128,64 @@ $(function() {
         if (SS.length < 2){SS = "0" + SS}
         return Number(String(YYYY) + MM + DD + HH + NN + SS);
     }
+    //チャットデータストア内の項目を一覧で取得
+    chatDataStore.stream().size(20).sort("desc").next(function(err, datas) {
+        datas.forEach(function(data) {
+            renderMessage(data);
+        });
+    });
+
+    chatDataStore.on("push", function(e) {
+        // チャット内容変更のイベントを受け取った際に、チャットメッセージを表示
+        renderMessage(e);
+    });
+
+    function renderMessage(msg) {
+        //取得したチャットデータストア内の項目一覧を画面上に表示
+
+        // var last_message = "summary";
+        // //取得したチャットデータストア内の項目一覧を画面上に表示
+        // //TODO:テーブルに書き換える
+        // var message_html = '<p class="post-text">' + msg.value.message + '</p>';
+        // var user_html    = '<p class="post-user">' + msg.value.userId + '</p>';
+        // var date_html    = '<p class="post-date">'+msg.value.date+'</p>';
+        // $("#"+last_message).after('<div id="'+msg.id+'" class="post">'
+        //     + user_html
+        //     + " : "
+        //     + message_html
+        //     + date_html
+        //     +'</div>');
+        // last_message = msg.id;
+
+        // ユーザごとにアイコンや表示を変更
+        // TODO:とりあえずベタ書き。後で動的にする。
+        if(msg.value.userId == "user1"){
+            var icon = "azarashi.png";
+            var classes = "chat-talk mytalk";
+            var target = "myicon";
+        }else{
+            var icon = "hakase.png";
+            var classes = "chat-talk";
+            var target = "tartgeticon";
+        }
+
+        // TODO:もう少し綺麗に実装できるはず
+        var last_message = "chat-frame";
+        $("#"+last_message).prepend(
+        '<p class="'
+        + classes
+        + '">'
+        + '<span class="talk-icon">'
+        + '<img src="../img/'
+        + icon
+        + '"'
+        + 'alt="'
+        + target
+        + '"/>'
+        + '</span>'
+        + '<span class="talk-content">'
+        + msg.value.message
+        + '</span>'
+        + '</p>');
+    };
 });
